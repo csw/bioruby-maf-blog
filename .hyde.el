bc605808ecd535acf84be448d8a3b7d1d77a88cb
Clayton Wheeler <cswh@umich.edu>
2012-05-13 15:04:45 -0400

    Integrated Hyde support.

---
+++ .hyde.el
@@ -0,0 +1,7 @@
+(setq hyde/git/remote "origin"
+      hyde-deploy-dir "public"
+      hyde-posts-dir "source/_posts"
+      hyde-drafts-dir "source/_drafts"
+      hyde/deploy-command "rake rsync"
+      hyde/jekyll-command "rake generate"
+      )
